36e286e4361d3b4010f7047d7a21199b6fcb93bb
abdrhmn09 <abdurrahmanmarzuki2@gmail.com>
2025-05-25 20:46:09 +0700

    koneksi db

--- index.js
+++ index.js
@@ -9,7 +9,15 @@ app.use(cors());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://your-mongodb-uri');
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  tls: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error('MongoDB connection error:', err);
+});
 
 const userSchema = new mongoose.Schema({
   username: String,
